e60889ee018ced1547c381a226ba8e65d3f01847
Boyan Marinov <boyan.marinov@yahoo.com>
2013-10-23 01:12:23 +0300

    justeeth

---
+++ JusTeeth.App/Scripts/messageSender.js
@@ -0,0 +1,35 @@
+﻿/// <reference path="Kendo/jquery.min.js" />
+/// <reference path="jquery.signalR-2.0.0-rc1.js" />
+
+var rooms = [];
+
+$(document).ready(function () {
+
+    $.connection.hub.start();
+
+    var chat = $.connection.chat;
+
+    $("#join-room").click(function () {
+        $(this).hide();
+        $("#only-joined-users").show();
+        var id = $(this).attr("data-group-id");
+
+        chat.server.joinRoom(id);
+    });
+
+    $('#send-message').click(function () {
+        var msg = $('#message').val();
+        chat.server.sendMessageToRoom(msg, rooms[0]);
+    });
+
+    chat.client.addMessage = addMessage;
+    chat.client.joinRoom = joinRoom;
+});
+
+function addMessage(message) {
+    $('#chat-window').append('<div>' + message + '</div>');
+}
+
+function joinRoom(room) {
+    rooms.push(room);
+}
